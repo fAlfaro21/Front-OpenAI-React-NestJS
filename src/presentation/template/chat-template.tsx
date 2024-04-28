9105c35a241983dfb1ed28f20ca3e00dc1247a16
Fernando <alfarogr@hotmail.com>
2024-04-28 13:30:46 +0200

    Add React Router Dom, Layout deesign, Routes, Tailwind, React Markdown, Chat design

---
+++ src/presentation/template/chat-template.tsx
@@ -0,0 +1,73 @@
+import { useState } from 'react';
+import { GptMessage, MyMessage, TypingLoader, TextMessageBox } from '../components';
+
+interface Message {
+  text: string;
+  isGpt: boolean;
+}
+
+export const ChatTemplate = () => {
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [messages, setMessages] = useState<Message[]>([]);
+
+  const handlePost = async( text: string ) => {
+    setIsLoading(true);
+    setMessages( (prev) => [...prev, { text: text , isGpt: false }] );
+
+    //Todo USE CASE
+
+    setIsLoading(false);
+
+    //Todo Añadir el mensaje de isGPT en true
+  };
+
+  return (
+    <div className="chat-container">
+      <div className="chat-messages">
+        <div className="grid grid-cols-12 gap-y-2">
+
+          {/* Bienvenida */}
+          <GptMessage text="Hola, puedes escribir tu texto en español, y te ayudo con las correcciones"/>
+          {
+            messages.map( (message, index) => (
+              message.isGpt
+                ? (
+                  <GptMessage key={ index } text="Esto es de OpenAI" />
+                )
+                : (
+                  <MyMessage key={ index } text={ message.text } />
+                )
+            ))
+          }
+          {
+            isLoading && (
+              <div className='col-start-1 col-end-12 fade-in'>
+                <TypingLoader />
+              </div>
+            )
+          }
+                
+
+        </div>
+      </div>
+
+      <TextMessageBox
+        onSendMessage={ handlePost }
+        placeholder='Escribe aquí lo que deseas'
+        disableCorrections
+      />
+
+      {/* <TextMessageBoxFile
+        onSendMessage={ handlePost }
+        placeholder='Escribe aquí lo que deseas'
+      /> */}
+
+      {/* <TextMessageBoxSelect 
+        onSendMessage={ console.log }
+        options={ [ { id:'1', text:'Hola' }, { id:'2' , text: 'Mundo' } ] }
+      /> */}
+
+    </div>
+  )
+}
